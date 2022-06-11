9df3f66597e724208fb51ae0168e106f43960a5f
Vico <vpontin93@gmail.com>
2022-06-10 21:16:04 -0300

    Avancei mais um pouco
    Graças ao LPK do server do ComputerCraft. Obrigado!

--- script.js
+++ script.js
@@ -7,7 +7,8 @@
  */
 
 /* BIBLIOTECAS */
-let Parser = require('rss-parser');
+const Parser = require('rss-parser');
+const fs = require("fs");
 
 /* VARIÁVEIS GLOBAIS */
 let parser = new Parser();
@@ -15,18 +16,34 @@ let parser = new Parser();
 // Lê o arquivo de config
 const config = require("./config.json");
 
+// Verifica se o arquivo last_timestamp não existe (primeira execução?)...
+if (!fs.existsSync("./last_timestamp"))
+{
+	// Cria o arquivo com o timestamp atual, pra poder realizar as checagens abaixo
+	console.log("[INFO] Arquivo last_timestamp não foi encontrado. Criando e registrando a data atual...");
+	fs.writeFileSync("./last_timestamp", new Date().getTime().toString());
+}
+
+// Lê a timestamp da última execução (ou a recém criada caso seja a primeira execução)
+let last_timestamp = new Date(parseInt(fs.readFileSync("./last_timestamp").toString())).getTime();
+// console.log(last_timestamp); DEBUG
+
+
 // Verifica cada feed do config...
 config.forEach(element => {	
 	parser.parseURL(element.rss_url, function(err, feed) {
-		// Nome do feed atual
-		console.log(element.name);
-		console.log(feed.title);
 		// Para cada entrada existente no feed...
-		feed.items.forEach(function(entry) {
-			if ()
+		feed.items.forEach(function(entry)
+		{
+			var item_timestamp = new Date(entry.pubDate).getTime();
+			// Se a sobra da data/hora do item menos a data/hora da última verificação é maior que zero (mais recente)
+			if (item_timestamp - last_timestamp > 0)
 			{
+				console.log(entry.title + ' : ' + entry.link + " - " + item_timestamp); // ADICIONAR LÓGICA DO WEBHOOK AQUI
 			}
-			console.log(entry.title + ' : ' + entry.link);
 		});
 	});
 });
+
+// Registra a timestamp desta execução para poder checar na próxima execução
+fs.writeFileSync("./last_timestamp", new Date().getTime().toString());
